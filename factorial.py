c4f089abc3d02f96daef9351d8a26f77ef061830
cyborg <abhi.bundela1996@gmail.com>
2015-10-04 16:32:20 +0530

    created factorial.py

---
+++ factorial.py
@@ -0,0 +1,18 @@
+# factorial calculation using recursion
+
+
+def factorial(n):
+	if n==0:
+		return 1
+	else:
+		return n * factorial(n-1)
+
+
+
+# algorithm analysis
+# time complexity O(n)
+# space complexity (1)
+
+
+
+
